8cb98499b807db74ff914bc60a1797c0512cdfe1
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-25 13:04:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  get '/' do 
+    erb :index
+  end
+  
+  post '/puppy' do 
+    erb :create_puppy
+  end
+  
+  post '/newpuppyinfo' do 
+    @puppy = Puppy.new(params[:user_input])
+    
+    erb :display_puppy
+  end
 end
